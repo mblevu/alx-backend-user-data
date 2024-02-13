9de20f31ee81e178489b95d1b9202d75d951e39d
mblevu <brianislevu@gmail.com>
2024-02-13 10:37:00 +0300

    implement Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -38,4 +38,4 @@ def not_allowed(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(debug=True, host=host, port=port)
