7b0c36ad7a5be6a49642a7763b6beeddc0c64109
mblevu <brianislevu@gmail.com>
2024-02-13 10:39:07 +0300

    Add require_auth method to Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,7 @@ from typing import List, TypeVar
 class Auth:
     """class to manage the API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth method"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
         if path[-1] != '/':
