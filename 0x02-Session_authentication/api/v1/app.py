45a4952fdb3c280a1bac34ca9ec0c70fd7995133
mblevu <brianislevu@gmail.com>
2024-02-14 21:54:31 +0300

    Refactor authentication logic in app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,9 +32,10 @@ def before_request() -> None:
                                             '/api/v1/unauthorized/',
                                             '/api/v1/forbidden/',
                                             '/api/v1/auth_session/login/']):
-            if not auth.authorization_header(request) and not auth.session_cookie(request):
-                abort(401)
-                return None
+            if not auth.authorization_header(request):
+                if not auth.session_cookie(request):
+                    abort(401)
+                    return None
             request.current_user = auth.current_user(request)
             if request.current_user is None:
                 abort(403)
