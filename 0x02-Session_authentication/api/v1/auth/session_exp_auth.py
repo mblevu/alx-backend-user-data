8ef99fe3368b6a50ff25773de1709b9f020e15ab
mblevu <brianislevu@gmail.com>
2024-02-15 15:01:40 +0300

    Add constructor and methods to SessionExpAuth class

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -9,13 +9,16 @@ class SessionExpAuth(SessionAuth):
     """Session expiration class"""
 
     def __init__(self) -> None:
+        """Constructor method"""
         super().__init__()
         self.session_duration = int(getenv('SESSION_DURATION', 0))
 
     def create_session(self, user_id: str = None) -> str:
+        """Create session method"""
         return super().create_session(user_id)
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """User id for session id method"""
         if session_id is None:
             return None
         session_dict = super().user_id_for_session_id(session_id)
