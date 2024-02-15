18f458ea062ad508cca69ce282dbea1a61f79c50
mblevu <brianislevu@gmail.com>
2024-02-15 15:00:53 +0300

    Add session expiration authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""session expiration"""
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+from os import getenv
+
+
+class SessionExpAuth(SessionAuth):
+    """Session expiration class"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.session_duration = int(getenv('SESSION_DURATION', 0))
+
+    def create_session(self, user_id: str = None) -> str:
+        return super().create_session(user_id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if session_id is None:
+            return None
+        session_dict = super().user_id_for_session_id(session_id)
+        if session_dict is None:
+            return None
+        if 'created_at' not in session_dict:
+            return None
+        if 'user_id' not in session_dict:
+            return None
+        if self.session_duration <= 0:
+            return session_dict['user_id']
+        before_time = session_dict[
+            'created_at'] + timedelta(seconds=self.session_duration)
+        if before_time < datetime.utcnow():
+            return None
+        return session_dict['user_id']
