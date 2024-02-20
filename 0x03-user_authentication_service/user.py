734517951bc44f0881503fa30ce1dfb2fa5e5d26
mblevu <brianislevu@gmail.com>
2024-02-20 20:33:25 +0300

    Remove primary key constraint from email column in User table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True, nullable=False)
-    email = Column(String(250), primary_key=True, nullable=False)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
