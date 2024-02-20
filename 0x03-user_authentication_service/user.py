ef21cf45a637e8f5bd93154e25925e884bd38de4
mblevu <brianislevu@gmail.com>
2024-02-20 23:08:17 +0300

    Add authentication module and user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,12 +2,20 @@
 """User model"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from typing import Optional
 
 Base = declarative_base()
 
 
 class User(Base):
+    """Class representing a user.
+
+    Attributes:
+        id (int): The unique identifier for the user.
+        email (str): The email address of the user.
+        hashed_password (str): The hashed password of the user.
+        session_id (str): The session ID of the user.
+        reset_token (str): The reset token of the user.
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
