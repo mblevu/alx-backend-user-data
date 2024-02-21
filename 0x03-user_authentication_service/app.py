228f9a4389b2d210b2f613eb2f5ed100f9abdbbe
mblevu <brianislevu@gmail.com>
2024-02-21 16:35:49 +0300

    Add logout functionality to app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -54,6 +54,17 @@ def login() -> str:
             response.set_cookie("session_id", session_id)
             return response, 200
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """logout a user"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/', code=302)
+    abort(403)
 
 
 if __name__ == "__main__":
