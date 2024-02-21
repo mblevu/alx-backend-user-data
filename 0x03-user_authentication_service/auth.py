2330f57704ba8714e6231eec3d9445c13ddd7e34
mblevu <brianislevu@gmail.com>
2024-02-21 16:24:26 +0300

    Add get_user_from_session_id method to Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """get user from session id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
