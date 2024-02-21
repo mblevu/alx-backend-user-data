fd83b893d04071022aadc1b0efa3ff83a4d80db3
mblevu <brianislevu@gmail.com>
2024-02-21 12:50:46 +0300

    Add UUID generation function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,6 +16,12 @@ def _hash_password(password: str) -> str:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """Generate a new UUID.
+    """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
