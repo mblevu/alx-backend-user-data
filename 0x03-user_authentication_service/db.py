9d3ea588450e7aa1cbc5b5d086be8fefafeb5ef7
mblevu <brianislevu@gmail.com>
2024-02-21 12:16:43 +0300

    Update DB class and add user authentication functionality

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,12 +3,12 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.exc import InvalidRequestError
-from user import User
-from user import Base
+from typing import TypeVar
+from user import Base, User
 
 
 class DB:
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,26 +33,30 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add a new user to db"""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """
+        Add a new user to the database.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         user = self._session.query(User).filter_by(email=email).first()
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user by the given keyword arguments
+        """
+        Find a user by the given criteria.
         """
         users = self._session.query(User)
         for key, value in kwargs.items():
             if key not in User.__dict__:
                 raise InvalidRequestError
-            for user in users:
-                if getattr(user, key) == value:
-                    return user
+            for usr in users:
+                if getattr(usr, key) == value:
+                    return usr
         raise NoResultFound
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Update a user by the given user_id
+        """
+        Update a user in the database.
         """
         try:
             user = self._session.query(User).filter_by(id=user_id).first()
@@ -60,5 +64,6 @@ class DB:
                 if key not in user.__dict__:
                     raise ValueError
                 setattr(user, key, value)
+            self._session.commit()
         except NoResultFound:
             raise ValueError
