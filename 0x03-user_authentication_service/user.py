eff31710f28582db93dbf92ab607847bd635faa7
mblevu <brianislevu@gmail.com>
2024-02-21 11:55:59 +0300

    Add Auth class and update User class documentation

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
-"""User model"""
+"""module defines a User class to be mapped"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
 
 class User(Base):
-    """Class representing a user.
+    """
+    Represents a user in the system.
 
     Attributes:
         id (int): The unique identifier for the user.
         email (str): The email address of the user.
         hashed_password (str): The hashed password of the user.
         session_id (str): The session ID of the user.
-        reset_token (str): The reset token of the user.
+        reset_token (str): The reset token for the user's password reset.
     """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
